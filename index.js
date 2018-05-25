db360762002da16481a848a5b9f8add21f8b61c3
LiBai666 <overwall2016@gmail.com>
2018-05-25 22:58:43 +0800

    允许添加数据

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var myAjax = function(target){
     return new Promise( (resolve,reject) => {
 
+        let method = target.method.toLocaleLowerCase()
+        let url = target.url
+        let formatData = ""
+        for(key in target.data){
+            formatData = formatData + `${key}=${target.data[key]}&`
+        }
+
+        switch(method){
+            case "get":
+                url = url + "?" + formatData 
+        }
+        
+
         let request = new XMLHttpRequest()
         request.onload = function () {
             if (request.status === 200) {
@@ -9,23 +22,33 @@ var myAjax = function(target){
                 reject(request.status)
             }
         }
-        request.open(target.method, target.url);
+        request.open(method,url);
 
         //设置请求头
         for(let key in target.header){
             request.setRequestHeader(key,target.header[key])
         }
-        request.send();
+
+        if(method == "post"){
+            request.send(formatData)
+        }else{
+            request.send();
+        }
+        
     }).catch( err => {
         console.log(new Error(err))
     })
 }
 
-let url = "https://www.iyingdi.cn/commentplus/list/article?web=1&size=20&id=0&article=56905"
+let url = "https://www.iyingdi.cn/commentplus/list/article"
 let target = {
     url:url,
     method:"GET",
     data:{
+        "web":1,
+        "size":20,
+        "id":0,
+        "article":56905
     },
     header:{
         uselessheader:"uselessheader"
