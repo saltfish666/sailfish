226c1cb6c60b966bed2c3a7d78e529dfc6cdac56
LiBai666 <overwall2016@gmail.com>
2018-05-25 21:57:05 +0800

    整理代码格式

---
+++ README.md
@@ -0,0 +1,5 @@
+## branch
+master
+release   -->   生产版本（未压缩）
+dev       -->   开发版本
+sailfish  -->   自己开发的版本
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+var myAjax = function(target){
+    return new Promise( (resolve,reject) => {
+
+        let request = new XMLHttpRequest()
+        request.onload = function () {
+            if (request.status === 200) {
+                resolve(request.responseText)
+            } else {
+                reject(request.status)
+            }
+        }
+        request.open(target.method, target.url);
+
+        //设置请求头
+        for(let key in target.header){
+            request.setRequestHeader(key,target.header[key])
+        }
+        request.send();
+    }).catch( err => {
+        console.log(new Error(err))
+    })
+}
+
+let url = "https://www.iyingdi.cn/commentplus/list/article?web=1&size=20&id=0&article=56905"
 let target = {
     url:url,
     method:"GET",
@@ -7,36 +31,6 @@ let target = {
         uselessheader:"uselessheader"
     }
 }
-
-var myAjax = function(url){
-    return new Promise( (resolve,reject) => {
-
-        let request = new XMLHttpRequest()
-        request.onload = function () {
-            console.log('111')
-            if (request.status === 200) {
-                console.log("222")
-                resolve(request.responseText)
-            } else {
-                reject(request.responseText)
-            }
-        }
-        request.open(target.method, target.url);
-
-        //设置请求头
-        for(let key in target.header){
-            request.setRequestHeader(key,target.header[key])
-        }
-
-        request.send();
-    }).then( (responseText) => {
-        return responseText
-    }).catch( err => {
-        console.log(err)
-    })
-}
-
-let url = "https://www.iyingdi.cn/commentplus/list/article?web=1&size=20&id=0&article=56905"
 
 myAjax(target).then( data =>{
     console.log(JSON.parse(data))
