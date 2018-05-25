3d02ffe02a6af523532de8c26de8539e323311bd
LiBai666 <overwall2016@gmail.com>
2018-05-25 22:08:21 +0800

    直接复制黏贴版

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
-var myAjax = function(target){
-    return new Promise( (resolve,reject) => {
+{
+    let myAjax = function(target){
+        return new Promise( (resolve,reject) => {
 
-        let request = new XMLHttpRequest()
-        request.onload = function () {
-            if (request.status === 200) {
-                resolve(request.responseText)
-            } else {
-                reject(request.status)
+            let request = new XMLHttpRequest()
+            request.onload = function () {
+                if (request.status === 200) {
+                    resolve(request.responseText)
+                } else {
+                    reject(request.status)
+                }
             }
-        }
-        request.open(target.method, target.url);
+            request.open(target.method, target.url);
 
-        //设置请求头
-        for(let key in target.header){
-            request.setRequestHeader(key,target.header[key])
-        }
-        request.send();
-    }).catch( err => {
-        console.log(new Error(err))
-    })
-}
-
-let url = "https://www.iyingdi.cn/commentplus/list/article?web=1&size=20&id=0&article=56905"
-let target = {
-    url:url,
-    method:"GET",
-    data:{
-    },
-    header:{
-        uselessheader:"uselessheader"
+            for(let key in target.header){
+                request.setRequestHeader(key,target.header[key])
+            }
+            request.send();
+        }).catch( err => {
+            console.log(new Error(err))
+        })
     }
+    var sailfish = myAjax
 }
-
-myAjax(target).then( data =>{
-    console.log(JSON.parse(data))
-})
