d242305d82b4bb43e2b5e6725d4c6c52e8828dd4
LiBai666 <overwall2016@gmail.com>
2018-05-26 14:14:11 +0800

    将api格式改为myAjax.get，添加jsonp函数

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-var myAjax = function(target){
+var myAjax = {}
+myAjax.get = function(target){
     return new Promise( (resolve,reject) => {
-
-        let method = target.method.toLocaleLowerCase()
-        let url = target.url
+        let method = "get"
         let formatData = ""
         for(key in target.data){
             formatData = formatData + `${key}=${target.data[key]}&`
         }
-
-        switch(method){
-            case "get":
-                url = url + "?" + formatData 
-        }
-        
+        let url = target.url + "?" + formatData
 
         let request = new XMLHttpRequest()
         request.onload = function () {
@@ -24,26 +18,62 @@ var myAjax = function(target){
         }
         request.open(method,url);
 
-        //设置请求头
         for(let key in target.header){
             request.setRequestHeader(key,target.header[key])
         }
 
-        if(method == "post"){
-            request.send(formatData)
-        }else{
-            request.send();
-        }
-        
-    }).catch( err => {
-        console.log(new Error(err))
+        request.send();
     })
+}
+
+myAjax.post = function(target){
+    return new Promise( (resolve,reject) => {
+        let method = "post"
+        let formatData = ""
+        for(key in target.data){
+            formatData = formatData + `${key}=${target.data[key]}&`
+        }
+        let url = target.url
+
+        let request = new XMLHttpRequest()
+        request.onload = function () {
+            if (request.status === 200) {
+                resolve(request.responseText)
+            } else {
+                reject(request.status)
+            }
+        }
+        request.open(method,url);
+
+        for(let key in target.header){
+            request.setRequestHeader(key,target.header[key])
+        }
+
+        request.send(formatData);
+    })
+}
+
+var myAjaxCallBackFun_123456_
+myAjax.jsonp = function(target){
+    
+    myAjaxCallBackFun_123456_ = target.callback
+    let method = "get"
+    let formatData = ""
+    for(key in target.data){
+        formatData = formatData + `${key}=${target.data[key]}&`
+    }
+    let url = `${target.url}?callback=myAjaxCallBackFun_123456_&${formatData}`
+    
+    let script = document.createElement('script');
+    script.setAttribute("type","text/javascript");
+    script.src = url;
+    document.body.appendChild(script)
 }
 
 let url = "https://www.iyingdi.cn/commentplus/list/article"
 let target = {
     url:url,
-    method:"GET",
+    callback:()=>{},
     data:{
         "web":1,
         "size":20,
